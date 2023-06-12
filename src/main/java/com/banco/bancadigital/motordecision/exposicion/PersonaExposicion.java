8beb580cfb9de77188a87aba1cd36e64107d92b5
cbaltaza <balta1_carlos@hotmail.com>
2023-06-12 12:12:06 -0600

    persona quarkus

--- src/main/java/com/banco/bancadigital/motordecision/exposicion/PersonaExposicion.java
+++ src/main/java/com/banco/bancadigital/motordecision/exposicion/PersonaExposicion.java
@@ -5,11 +5,14 @@ package com.banco.bancadigital.motordecision.exposicion;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +28,8 @@ import com.banco.bancadigital.motordecision.service.PersonaService;
 @CrossOrigin(origins = { "http://localhost:80", "http://localhost:4200", "http://localhost:8080", "http://localhost" })
 public class PersonaExposicion {
 
+    public static final Logger log = LoggerFactory.getLogger(PersonaExposicion.class);
+
 	@Autowired
 	private PersonaService service;
 
@@ -34,7 +39,9 @@ public class PersonaExposicion {
 	}
 
 	@GetMapping("/get/{id}")
-	public Person getProduct(@PathVariable("id") Long id) {
+	public Person getProduct(@PathVariable("id") Long id, @RequestHeader("token") String token) {
+		
+		log.info("Datos de entrada {}, {}.", id, token);
 		return service.recuperarPerson( id );
 	}
 
